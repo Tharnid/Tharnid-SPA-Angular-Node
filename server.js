a5b4b3d7016004c74b4178d9bb47fc30da9a0d5d
Tharnid <jeffmsmithdev@gmail.com>
2013-12-05 23:40:07 -0500

    done with the angular part...on to part two

--- server.js
+++ server.js
@@ -73,6 +73,11 @@
 		});
 	});
 
+	// application
+	app.get('*', function(req, res) {
+		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+	});
+
 	// listen (start app with node server.js) ======================================
 	app.listen(4040);
 	console.log("App listening on port 4040");
